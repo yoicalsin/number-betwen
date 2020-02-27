8be3fab49f593c0550d343d92efbd02d839bd481
Yoni Calsin <yonicb20@gmail.com>
2020-02-26 19:56:05 -0500

    Removed all !

--- src/index.js
+++ src/index.js
@@ -1,9 +1,3 @@
-import "./styles.css";
+const number = 92;
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use Parcel to bundle this sandbox, you can find more info about Parcel
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+console.log(number);
